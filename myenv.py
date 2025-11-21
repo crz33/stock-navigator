f136c930231fcca8973b65b561f1dca9afc1e2fa
crz33 <takada.masaharu@gmail.com>
2025-11-22 06:53:11 +0900

    KABU+のアクセス情報のコメントを追加

--- myenv.py
+++ myenv.py
@@ -4,5 +4,6 @@ import pathlib
 
 dotenv.load_dotenv(pathlib.Path(__file__).parent.joinpath(".env"))
 
+# KABU+のアクセス情報
 KABU_PLUS_USER = os.environ.get("KABU_PLUS_USER")
 KABU_PLUS_PASS = os.environ.get("KABU_PLUS_PASS")
